7d33365c2fb5cefb0714a1a66688b4a3615379bd
Carla Holcomb <carla.programmer@gmail.com>
2020-02-04 03:12:25 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -2,12 +2,12 @@ class Backer
   attr_reader :backed_projects, :name
   
   def initialize(backed_projects)
-    @name = name
-    @backed_projects = []
+    @backed_projects = backed_projects
+    @@backed_projects = []
   end
   
   def back_project(backed_projects)
-    @backed_projects << backed_projects
+    @@backed_projects << backed_projects
     
   end
   
